5cdbbeb42cee222e7e861a8aaf08c6df22734efb
Ryan Phillips <ryan.phillips@rackspace.com>
2012-01-26 17:30:20 -0800

    bump version

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "async",
-  version = "0.0.1",
+  version = "0.0.2",
   description = "async library for luvit",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
