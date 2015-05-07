f9ed0c9c0e6be84e9fa25d7f5f3e256d3126f847
Ryan Phillips <ryan.phillips@rackspace.com>
2015-05-07 13:28:37 -0500

    only include lua files

--- package.lua
+++ package.lua
@@ -1,7 +1,12 @@
 return {
   name = "virgo-agent-toolkit/async",
-  version = "1.0.2",
+  version = "1.0.3",
   description = "async library for luvit",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
-  dependencies = {},
+  dependencies = {
+    "luvit/luvit@2"
+  },
+  files = {
+    "*.lua"
+  }
 }
